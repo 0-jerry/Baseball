333aa8d2ec225583e832c30b53e88ffa8b1e17b8
이재영 <ldc1000@naver.com>
2024-11-08 08:06:47 +0900

    chore: description 수정 및 주석 추가

--- BaseBall/GameResult.swift
+++ BaseBall/GameResult.swift
@@ -10,16 +10,26 @@ import Foundation
 //MARK: - 게임 결과 데이터
 struct GameResult: CustomStringConvertible {
     
-    private var countOfTry: Int {
-        judgeResults.count
-    }
+    //승패
+    private var isWin: Bool = false
+    
+    //총 시도횟수 연산
+    private var countOfTry: Int { judgeResults.count }
+    
+    //판단 결과 저장
     private var judgeResults: [JudgeResult] = []
     
     var description: String {
-        "시도 횟수 - \(countOfTry)"
+        self.isWin ? "[성공] - \(countOfTry)회 시도" : "[실패]"
     }
     
+    //판단 결과 추가 메서드
     mutating func addJudgeResult(_ judgeResult: JudgeResult) {
         self.judgeResults.append(judgeResult)
+    }
+    
+    //성공할 경우 사용될 메서드
+    mutating func win() {
+        self.isWin = true
     }
 }
