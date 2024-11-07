4c86519315c64b4a43aabf9ec3a0d06fa95e13af
이재영 <ldc1000@naver.com>
2024-11-08 08:42:53 +0900

    chore: 주석 수정

--- BaseBall/GameHistory.swift
+++ BaseBall/GameHistory.swift
@@ -7,18 +7,22 @@
 
 import Foundation
 
-//MARK: - 게임 기록 데이터
+//MARK: - 게임 기록 데이터 (싱글톤)
 class GameHistory: CustomStringConvertible {
     
+    private init() {}
+
     static let shared = GameHistory()
     
-    private init() {
-    }
-    
+    //게임 기록 배열 형태로 저장
     private var gameResults: [GameResult] = []
     
+    //게임 기록 출력을 위한 연산
     var description: String {
-        print(self.gameResults.count)
+        guard !gameResults.isEmpty else {
+            return "기록이 존재하지 않습니다."
+        }
+        
         var historyMessages: [String] = []
         
         for i in 0..<gameResults.count {
@@ -30,9 +34,8 @@ class GameHistory: CustomStringConvertible {
         return historyMessages.joined(separator: "\n")
     }
     
-    
+    //게임 라운드가 종료될 경우 사용될 메서드
     func addGameResult(_ gameResult: GameResult) {
         gameResults.append(gameResult)
     }
-    
 }
